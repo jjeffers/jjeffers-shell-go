11bf7024ea3befee7f575b82ad95c042c20f3303
James Jeffers <jdjeffers@protonmail.com>
2024-12-02 22:16:41 -0500

    Invalid command error message.

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
-	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	cmd = strings.TrimSpace(cmd)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s: command not found\n", cmd)
 }
+

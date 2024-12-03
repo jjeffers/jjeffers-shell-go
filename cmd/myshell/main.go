86ca5122f5bcd178736172c4a5226ddd8a9b75a4
James Jeffers <jdjeffers@protonmail.com>
2024-12-02 22:47:05 -0500

    Implements exit command.

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,23 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+func handleInput(reader *bufio.Reader) {
+
+	input, err := reader.ReadString('\n')
+
+	input = strings.TrimSpace(input)
+
+	cmd_args := strings.Split(input, " ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := strings.ToLower(cmd_args[0])
+
+	switch cmd {
+	case "exit":
+		exit_status, err := strconv.Atoi(cmd_args[1])
+		if err != nil {
+			fmt.Errorf("Can't convert '%s' to valid exit status.", cmd_args[1])
+		}
+		os.Exit(exit_status)
+	default:
+		fmt.Printf("%s: command not found\n", cmd)
+	}
+}
+
 func main() {
 	for {
-			fmt.Fprint(os.Stdout, "$ ")
+		fmt.Fprint(os.Stdout, "$ ")
 
-			// Wait for user input
-			cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
-			cmd = strings.TrimSpace(cmd)
-
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("%s: command not found\n", cmd)
-		}
+		handleInput(bufio.NewReader(os.Stdin))
+	}
 
 }

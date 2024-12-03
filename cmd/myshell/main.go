0da8dfdb1d6017908865295c65a6c9653e37c836
James Jeffers <jdjeffers@protonmail.com>
2024-12-03 11:12:49 -0500

    Implements builtin checking, refactor to use a map of functions

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,7 +8,40 @@ import (
 	"strings"
 )
 
+type shell_func func(cmd_map map[string]shell_func, args []string)
+
+func exitCmd(command_map map[string]shell_func, args []string) {
+
+	exit_status, err := strconv.Atoi(args[0])
+	if err != nil {
+		panic(fmt.Errorf("Can't convert '%s' to valid exit status.", args[0]))
+
+	}
+	os.Exit(exit_status)
+}
+
+func echoCmd(command_map map[string]shell_func, args []string) {
+	echo_args := strings.Join(args, " ")
+	fmt.Printf("%s\n", echo_args)
+}
+
+func typeCmd(command_map map[string]shell_func, args []string) {
+	cmd := args[0]
+
+	if _, exists := command_map[cmd]; exists {
+		fmt.Printf("%s is a shell builtin\n", cmd)
+	} else {
+		fmt.Printf("%s: not found\n", cmd)
+	}
+}
+
 func handleInput(reader *bufio.Reader) {
+
+	commands := map[string]shell_func{
+		"exit": exitCmd,
+		"echo": echoCmd,
+		"type": typeCmd,
+	}
 
 	input, err := reader.ReadString('\n')
 
@@ -22,17 +55,11 @@ func handleInput(reader *bufio.Reader) {
 
 	cmd := strings.ToLower(cmd_args[0])
 
-	switch cmd {
-	case "exit":
-		exit_status, err := strconv.Atoi(cmd_args[1])
-		if err != nil {
-			panic(fmt.Errorf("Can't convert '%s' to valid exit status.", cmd_args[1]))
-		}
-		os.Exit(exit_status)
-	case "echo":
-		echo_args := strings.Join(cmd_args[1:], " ")
-		fmt.Printf("%s\n", echo_args)
-	default:
+	if matched_cmd, exists := commands[cmd]; exists {
+
+		matched_cmd(commands, cmd_args[1:])
+
+	} else {
 		fmt.Printf("%s: command not found\n", cmd)
 	}
 }
